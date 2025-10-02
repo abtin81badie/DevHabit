44a22bfdbe49e7a28b03cea039119590f27d77d3
abtin81badie <abtinbadiee1328@gmail.com>
2025-10-02 19:53:10 +0330

    feat: Introduction the Database.

---
+++ DevHabit/DevHabit.Api/Database/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DevHabit.Api.Database;
+
+public sealed class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDefaultSchema(Schemas.Application);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+}
+
+public static class Schemas
+{
+    public const string Application = "dev_habit";
+}
---
+++ DevHabit/DevHabit.Api/Extensions/DatabaseExtensions.cs
@@ -0,0 +1,27 @@
+﻿using DevHabit.Api.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevHabit.Api.Extensions;
+
+public static class DatabaseExtensions
+{
+    public static async Task ApplyMigrationsAsync(this WebApplication app) 
+    {
+        using IServiceScope scope = app.Services.CreateScope();
+        await using ApplicationDbContext dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+        try
+        {
+            await dbContext.Database.MigrateAsync();
+
+            app.Logger.LogInformation("Database ,mirations applied successfully.");
+        }
+        catch (Exception ex) 
+        {
+            app.Logger.LogError(ex, "An error occured while applying databse migrations.");
+            throw;
+        }
+
+        
+    }
+}
