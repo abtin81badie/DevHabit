c91c0a2436921489a9a22082bda4831992c10c04
abtin81badie <abtinbadiee1328@gmail.com>
2025-10-11 16:13:30 +0330

    feat: Implementing  user registration.

---
+++ DevHabit/DevHabit.Api/DTOs/Users/UserMappings.cs
@@ -0,0 +1,19 @@
+﻿using DevHabit.Api.DTOs.Auth;
+using DevHabit.Api.Entities;
+
+namespace DevHabit.Api.DTOs.Users;
+
+public static class UserMappings
+{
+    public static User ToEntity(this RegisterUserDto registerUserDto, string identityId)
+    {
+        return new User
+        {
+            Id = $"u_{Guid.CreateVersion7()}",
+            Name = registerUserDto.Name,
+            Email = registerUserDto.Email,
+            CreatedAtUtc = DateTime.UtcNow,
+            IdentityId = identityId
+        };
+    }
+}
